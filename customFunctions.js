39e6b70e48b605ebba9b4b5ceab2adcc16edc358
RickyC0626 <rickychon99@gmail.com>
2020-05-30 16:00:07 -0400

    Fix typos and long sentences

--- customFunctions.js
+++ customFunctions.js
@@ -36,7 +36,8 @@ console.log("\n\n");
 
 /**
  * @function
- * myMap: creates a new array populated with the results of calling a provided function on every element in the calling array
+ * myMap: creates a new array populated with the results of calling a provided function on every element 
+ * in the calling array
  * 
  * @param {Array} arr Array to perform the operation on
  * @param {Function} callback Function to execute on each element
@@ -71,9 +72,11 @@ console.log("\n\n");
  * myFilter: creates a new array with all elements that pass the test implemented by the provided function
  * 
  * @param {Array} arr Array to perform the operation on
- * @param {Function} callback Function to test each element of the array. Return true to keep the element, false otherwise
+ * @param {Function} callback Function to test each element of the array. 
+ * Return true to keep the element, false otherwise
  * 
- * @returns {Array} A new array with the elements that pass the past. If no elements pass the test, an empty array will be returned
+ * @returns {Array} A new array with the elements that pass the past. If no elements pass the test, 
+ * an empty array will be returned
  */
 function myFilter(arr, callback)
 {
@@ -105,7 +108,8 @@ console.log("\n\n");
  * @param {Array} arr Array to perform the operation on
  * @param {Function} callback Function to test for each element
  * 
- * @returns {boolean} true if the callback function returns a truthy value for at least one element in the array. Otherwise, false
+ * @returns {boolean} true if the callback function returns a truthy value for at least one element in the array. 
+ * Otherwise, false
  */
 function mySome(arr, callback)
 {
@@ -166,7 +170,8 @@ console.log("\n\n");
 
 /**
  * @function
- * myReduce: executes a reducer function (that you provide) on each element of the array, resulting in a single output value
+ * myReduce: executes a reducer function (that you provide) on each element of the array, 
+ * resulting in a single output value
  * 
  * @param {Array} arr Array to perform the operation on
  * @param {Function} callback Function to test for each element
@@ -183,7 +188,8 @@ function myReduce(arr, callback, initialValue)
     {
         if(index === 0) (initialValue !== undefined || null) ? accumulator = initialValue : accumulator = 0;
         // Callback can have 4 parameters:
-        // - accumulator: The accumulated return values of callback. It is the accumulated value previously returned in the last invotation of the callback, or initialValue, if it was supplied
+        // - accumulator: The accumulated return values of callback. It is the accumulated value previously returned 
+        // in the last invocation of the callback, or initialValue, if it was supplied
         // - currentValue: The current element being processed in the array
         // - index (optional): The index of currentValue being processed in the array
         // - array (optional): The array myReduce() was called upon
@@ -210,13 +216,16 @@ console.log("\n\n");
 
 /**
  * @function
- * myIncludes: determines whether an array includes a certain value among its entries, returning true or false as appropriate
+ * myIncludes: determines whether an array includes a certain value among its entries, 
+ * returning true or false as appropriate
  * 
  * @param {Array} arr Array to perform the operation on
  * @param {Object} valueToFind The value to search for
- * @param {Number} fromIndex (Optional) The position in the array which to begin searching for valueToFind. Defaults to 0
+ * @param {Number} fromIndex (Optional) The position in the array which to begin searching for valueToFind. 
+ * Defaults to 0
  * 
- * @returns {Boolean} which is true if the value valueToFind is found within the array (or the part of the array indicated by the index fromIndex, if specified)
+ * @returns {Boolean} which is true if the value valueToFind is found within the array 
+ * (or the part of the array indicated by the index fromIndex, if specified)
  */
 function myIncludes(arr, valueToFind, fromIndex)
 {
@@ -243,7 +252,8 @@ console.log("\n\n");
  * 
  * @param {Array} arr Array to perform the operation on
  * @param {Object} searchElement Element to locate in the array
- * @param {Number} fromIndex (Optional) The index to start the search at. If the index is greater than or equal to the array's length, -1 is returned, which means the array will not be searched
+ * @param {Number} fromIndex (Optional) The index to start the search at. If the index is greater than or equal to 
+ * the array's length, -1 is returned, which means the array will not be searched
  * 
  * @returns {Number} the first index of the element in the array, -1 if not found
  */
@@ -297,11 +307,13 @@ console.log("\n\n");
 
 /**
  * @function
- * myLastIndexOf: returns the last index at which a given elment can be found in the array, or -1 if it is not present. The array is searched backwards, starting at fromIndex
+ * myLastIndexOf: returns the last index at which a given elment can be found in the array, or -1 if it is not present. 
+ * The array is searched backwards, starting at fromIndex
  * 
  * @param {Array} arr Array to perform the operation on
  * @param {Object} searchElement Element to locate in the array
- * @param {Number} fromIndex (Optional) The index at which to start searching backwards. Defaults to (arr.length - 1). If the index is greater than or equal to the length of the array, the whole array will be searched
+ * @param {Number} fromIndex (Optional) The index at which to start searching backwards. Defaults to (arr.length - 1). 
+ * If the index is greater than or equal to the length of the array, the whole array will be searched
  * 
  * @returns {Number} the last index of the element in the array, -1 if not found
  */
@@ -330,7 +342,8 @@ console.log("\n\n");
 
 /**
  * @function
- * grabKeys: returns an array of a given object's own enumerable property 'names', iterated in the same order that a normal loop would
+ * grabKeys: returns an array of a given object's own enumerable property 'names', iterated in the 
+ * same order that a normal loop would
  * 
  * @param {Object} obj The object of which the enumerable's own properties are to be returned
  * 
@@ -372,7 +385,8 @@ console.log("\n\n");
 
 /**
  * @function
- * grabValues: returns an array of a given object's own enumerable property values, in the same order as that provided by a for...in loop
+ * grabValues: returns an array of a given object's own enumerable property values, in the same order as that is 
+ * provided by a for...in loop
  * 
  * @param {Object} obj The object of which the enumerable's own properties are to be returned
  * 
@@ -422,7 +436,8 @@ console.log("\n\n");
 
     Next, write a sum function that takes an array of numbers and returns the sum of these numbers.
 
-    As a bonus, modify the range function to take an optional third argument that indicates the "step" value used when building the array. 
+    As a bonus, modify the range function to take an optional third argument that indicates the 
+    "step" value used when building the array. 
     If no step is given, the elements go up by increments of one, corresponding to the old behavior.
 
     The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]
