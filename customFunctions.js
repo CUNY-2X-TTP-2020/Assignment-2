710368bad884393a4caa2a45c025465570d64627
RickyC0626 <rickychon99@gmail.com>
2020-05-29 22:58:28 -0400

    Fix typo in first misc problem

--- customFunctions.js
+++ customFunctions.js
@@ -404,7 +404,7 @@ console.log("\n\n");
     The Sum of a Range
 
     Write a range function that takes two arguments, start and end, and 
-    returns and array containing all the numbers from start up to (and including) end.
+    returns an array containing all the numbers from start up to (and including) end.
 
     Next, write a sum function that takes an array of numbers and returns the sum of these numbers.
 
