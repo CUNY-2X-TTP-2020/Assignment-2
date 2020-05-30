ddd60dd73beb1732c5829a5b393cbd8afef07f83
RickyC0626 <rickychon99@gmail.com>
2020-05-29 22:56:08 -0400

    Add instructions for first misc problem 'Sum of a Range'

--- customFunctions.js
+++ customFunctions.js
@@ -393,3 +393,24 @@ console.log("obj values: " + grabValues(obj));
 console.log("randObj values: " + grabValues(randObj));
 
 console.log("\n\n");
+
+/* 
+   ==============================================================================
+    Miscellaneous Problems: EJS Chapter 4 - Data Structures (Objects and Arrays)
+   ==============================================================================
+*/
+
+/*
+    The Sum of a Range
+
+    Write a range function that takes two arguments, start and end, and 
+    returns and array containing all the numbers from start up to (and including) end.
+
+    Next, write a sum function that takes an array of numbers and returns the sum of these numbers.
+
+    As a bonus, modify the range function to take an optional third argument that indicates the "step" value used when building the array. 
+    If no step is given, the elements go up by increments of one, corresponding to the old behavior.
+
+    The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]
+    Make sure it also works with negative step values so that the range(5, 2, -1) produces [5, 4, 3, 2]
+*/
